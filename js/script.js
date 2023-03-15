eca4f8104286130848fc2b842c807592e88b701d
post-coder <gabrielspanu96@gmail.com>
2023-03-15 12:28:23 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+
+const { createApp } = Vue;
+
+createApp({
+  // una lista di todo
+  // che posso vedere e eliminare quelli che ho già fatto
+
+  data() {
+    return {
+
+      todos: [
+        'Fare ricarica al telefono',
+        'Portare il cane al parco',
+        "Pushare l'esercizio del mattino",
+        'Inviare la consegna del nuovo esercizio',
+        "Chiedere l'età a Danilo",
+      ],
+      
+      // predispongo una proprietà per il nuovo todo
+      newTodo: '',
+      
+    }
+  },
+
+  methods: {
+    deleteTodo(index) {
+
+      // elimina l'elemento dall'array all'indice che ti do io
+      this.todos.splice(index, 1);
+
+    },
+
+    addTodo() {
+      // inserire il contenuto di "newTodo" dentro il mio array di todo
+      this.todos.push(this.newTodo);
+      this.newTodo = "";
+      
+    },
+
+  },
+
+}).mount('#app')
